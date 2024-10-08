5b70397a49a1229f30cb90f2b4b3296273ded5b9
Mark Anthony Perez <mark@act2.com.au>
2024-10-08 15:42:13 +0800

    Line of Business

--- Models/LineBusiness.cs
+++ Models/LineBusiness.cs
@@ -7,11 +7,17 @@ namespace OBLS.Models
     public class LineBusiness
     {
         public Guid Id { get; set; }
+        [DisplayName("Code")]
         public string? Code { get; set; }
+        [DisplayName("Description")]
         public string? Description { get; set; }
+        [DisplayName("Signage Billboard Capacity")]
         public string? SignageBillboard_Capacity { get; set; }
+        [DisplayName("Signage Billboard No. of Units")]
         public string? SignageBillboard_NoOfUnits { get; set; }
+        [DisplayName("Weights and Measures Capacity")]
         public string? WeightsAndMeasures_Capacity { get; set; }
+        [DisplayName("Weights and Measures No. of Units")]
         public string? WeightsAndMeasures_NoOfUnits { get; set; }
     }
 }
